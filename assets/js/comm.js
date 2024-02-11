64588cdb18df3ce005e27732642275b85e85df98
chohyang kim <chkim9910@naver.com>
2024-02-11 22:51:33 +0900

    update

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -23,14 +23,14 @@ $(function () {
     afterLoad: function (origin, destination, direction) {
       // destination.item이 현재 섹션을 나타냅니다.
       // 원하는 섹션에 대한 조건을 설정하여 해당 섹션이 화면에 들어왔을 때 동작을 수행합니다.
+      if (destination.index == 0) {
+      }
       if (destination.index == 1) {
         // 두 번째 섹션이 화면에 들어왔을 때 실행할 코드를 작성합니다.
         console.log("두 번째 섹션이 화면에 들어옵니다!");
-
+        const sect2Inner = document.querySelector(".sect2 .inner");
+        sect2Inner.classList.add("show");
         // typing animation
-        // function onComplete() {
-        //   gsap.to("#text", { y: "-50px", autoAlpha: 1, duration: 1 });
-        // }
         const text = document.getElementById("text");
         const typingDelay = 150;
         gsap.registerPlugin(ScrollTrigger);
@@ -44,10 +44,8 @@ $(function () {
           }
           const currentNode = nodes[index];
           const currentNodeText = currentNode.innerHTML;
-
           currentNode.innerHTML = "";
           target.appendChild(currentNode);
-
           // 만약 현재 노드의 내용이 있다면
           if (currentNodeText) {
             let i = 0;
@@ -55,10 +53,8 @@ $(function () {
             const effect = setInterval(() => {
               currentNode.innerHTML += cars[i];
               i++;
-
               if (i === cars.length) {
                 clearInterval(effect);
-
                 // 만약 현재 노드가 마지막 노드가 아니라면 다음 노드 처리
                 if (index < nodes.length - 1) {
                   displayText(target, delay, callback, nodes, ++index);
@@ -78,7 +74,6 @@ $(function () {
             }
           }
         }
-
         // 텍스트가 타이핑 애니메이션이 모두 완료된 후에 실행될 함수
         const secondText = document.getElementsByClassName("txt-sec-box");
         const bgCircle = document.getElementsByClassName("bg-circle");
@@ -87,10 +82,20 @@ $(function () {
         function onComplete() {
           console.log("콜백 함수가 실행되었습니다.");
           const tl = gsap.timeline();
-          tl.to(text, { y: -50, autoAlpha: 0, duration: 1, delay: 0.5 })
-            .to(bgCircle, { scale: 50, duration: 2, ease: "power2.out" })
+          tl /* .to(text, { y: -50, autoAlpha: 0, duration: 1, delay: 0.5 }) */.to(
+            bgCircle,
+            {
+              scale: 50,
+              duration: 1.5,
+              ease: "power2.out",
+              delay: 1,
+            }
+          )
             .to(secondText, { y: 0, autoAlpha: 1, duration: 1 });
         }
+      }
+      if (destination.index == 2) {
+        console.log("세 번째 섹션이 화면에 들어옵니다!");
       }
     },
   });
